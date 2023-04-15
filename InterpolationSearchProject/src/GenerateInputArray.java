d33152f10feafe01cfc5e2d33c9aeb46d6f9fa35
Caleb Krauter <krautercaleb@gmail.com>
2023-04-14 23:05:58 -0700

    (extracurricular - for hw prep) Implementation of Interpolation Search and Randomized Array as input

---
+++ InterpolationSearchProject/src/GenerateInputArray.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+public class GenerateInputArray {
+
+    private int[] inputArray;
+    private int size = 10;
+    private int element = 0;
+    private int min = 1;
+    private int max = 100;
+    private Random random;
+
+    // TODO make sure bug doesn't happen where 0 is printed after a larger number
+
+    public GenerateInputArray() {
+        random = new Random();
+        inputArray = new int[generateArraySize()];
+        fillArray(inputArray);
+        printArray();
+//        new InterpolationSearch();
+
+    }
+
+    // Generate array size between 50 and 5000
+    private int generateArraySize() {
+        size = random.nextInt(50, 5000);
+        return size;
+    }
+
+    private void fillArray(int[] inputArray) {
+       // Generate value between min = prevVal and max, if value is less than value at prevIndex,min = min + prevVal + 1
+
+        int prevElement = 0;
+        min = 0;
+        max = 10;
+        for (int i = 0; i < inputArray.length ; i++) {
+            element = random.nextInt(0, 10);
+
+            if (element > prevElement) {
+                inputArray[i] = element;
+            } else if (prevElement > element) {
+                element = element + prevElement + 1;
+                min = element;
+                max = element*2 + prevElement + 1;
+                inputArray[i] = element;
+            }
+            // TODO fix bug, why can't origin be min and bound be max? Doesn't work.
+            // THIS COMMENTED block might fix the bug! Not certain.
+//            if (i > 0 && prevElement > inputArray[i] && inputArray[i] == 0) {
+//
+//                inputArray[i] = element*2 + prevElement + 1;
+//            }
+            prevElement = element;
+        }
+    }
+
+    private void printArray() {
+        for (int element: inputArray) {
+            System.out.print(element + ", ");
+        }
+    }
+
+    public int[] getInputArray() {
+        return inputArray;
+    }
+}
---
+++ InterpolationSearchProject/src/InterpolationSearch.java
@@ -0,0 +1,43 @@
+public class InterpolationSearch {
+
+    GenerateInputArray generateInputArray;
+    private int low = 0;
+    private int[] inputArray = new GenerateInputArray().getInputArray();
+    private int high = inputArray.length - 1;
+    private int k = 50; // Target value
+
+    // TODO - I observed that when the target value does not exist in the set the interpolation search tries many positions,
+    //          many of which appear to be the same position, sometimes going back and forth in direction before giving up.
+    //          Is the algorithm wokring as intended or is this a bug?
+    // TODO - Fix bug where rarely, the array will generate a 0 in the wrong place meaning it will be after a larger value in order
+    //          ruining the order.
+
+    public InterpolationSearch() {
+        generateInputArray = new GenerateInputArray();
+        interpolationSearch(k, low, high);
+    }
+
+    private int interpolationSearch(int k, int low, int high) {
+
+        while (k >= inputArray[low] && k <= inputArray[high] && low <= high) {
+            int probPos = (int) (low + Math.floor(((k - inputArray[low]) * (high - low)) / (inputArray[high] - inputArray[low])));
+
+            System.out.println("Tried pos: " + probPos);
+
+            if (k == inputArray[probPos]) {
+                System.out.println("Found target k at pos: " + probPos);
+                return probPos;
+            }
+
+            if (k > inputArray[low]) {
+                low = low + 1;
+            } else if (k < inputArray[high]){
+                high = high - 1;
+            }
+        }
+            System.out.println("Target never found, return: -1");
+            return -1; // not found
+
+    }
+
+}
